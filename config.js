94f0f6563acc5276b7b56ae251423882f8ddcc06
Miroslaw Chmielecki <misiak19@op.pl>
2019-08-31 08:24:42 +0200

    Add ready files

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "908448042530-gn3p05dg4pkla1m1j9vu5234gvg46viv.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "fWWhINcDdUQa3xhK0hMof1QO",
+  CALLBACK_URL: "https://localhost:3000/auth/google/callback"
+};
